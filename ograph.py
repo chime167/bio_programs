600f57c41444e5bec9940fde165decfff186d9fa
chime167 <chimerame@protonmail.com>
2022-09-02 19:38:16 -0400

    added filename and overlap length arguments

--- ograph.py
+++ ograph.py
@@ -2,36 +2,47 @@
 
 def main():
     import re
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', help='Enter the filename or path to file')
+    parser.add_argument('overlap', help='Enter the length of the overlap e.g. 3 for AGT AGT')
+    args = parser.parse_args()
     results = []
     seq = ''
-    f = open('rosalind_grph.txt')
-    for line in f:
-        line = line.rstrip()
-        m = re.match('>([\s\S]+)', line)
-        if m:
-            dic = {'header': m.group(1), 'seq': None}
-            seq = ''
-            results.append(dic)
-        else:
-            seq = seq + line
-            dic['seq'] = seq 
-    
+    with open(args.filename) as f:
+        for line in f:
+            line = line.rstrip()
+            m = re.match('>([\s\S]+)', line)
+            if m:
+                dic = {'header': m.group(1), 'seq': None}
+                seq = ''
+                results.append(dic)
+            else:
+                seq = seq + line
+                dic['seq'] = seq 
+        
     header = [sub['header'] for sub in results]
     sequence = [sub['seq'] for sub in results]
     
-    k = 3
+    k = int(args.overlap)
     s = []
     t = []
+    first_half = []
+    second_half = []
 
-    for i,y in enumerate(header):
-        for j,z in enumerate(sequence):
+    for i, _ in enumerate(header):
+        for j, _ in enumerate(sequence):
             if i != j:
                 if sequence[i][-k:] == sequence[j][0:k]:
                     s.append(header[i])
                     t.append(header[j])
+                    first_half.append(sequence[i])
+                    second_half.append(sequence[j])
     
-    for x,y in enumerate(s):
-        print(s[x], t[x])
+    # for x, y in zip(s, t):
+    #     print(x, y)
+    for x, y in zip(first_half, second_half):
+        print(x + y[3:] + '\n')
 
             
 
